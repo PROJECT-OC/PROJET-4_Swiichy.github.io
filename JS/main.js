5aa4c50e4b3e1624a4e0a75940671332309a2e8c
Jansana Dylan <BOOMHEADSHOTMn@gmail.com>
2020-09-21 12:44:17 +0200

    Refonte de la maquette du site (ajout de la page d'acceuil)

---
+++ JS/main.js
@@ -0,0 +1,34 @@
+/*Récupération de l'id des différente page teddies
+
+window.addEventListener('load', onPageLoad);
+function getId(){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id = urlParams.get('id');
+    return id
+};
+
+function onPageLoad(){
+    const id = getId()
+    console.log(id)
+};
+Récupération de l'id des différente page teddies*/
+
+
+/*Recuperation de l'api teddies
+fetch("http://localhost:3000/api/teddies")
+    .then(res => res.json())
+    .then(res => {
+        console.log(res)
+    });*/
+
+
+
+
+
+
+
+
+
+
+
